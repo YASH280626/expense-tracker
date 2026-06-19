d213e7002531d5008f6b6281b8cac0d8de956d4d
YASH280626 <shreyanshpathak53@gmail.com>
2026-06-19 08:25:08 +0530

    Save expenses to text file

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ while True:
     print("2. Exit")
     choice = int(input("Enter Your Choice: "))
     if(choice == 1):
-        expense = int(input("Enter Expense"))
+        expense = int(input("Enter Expense: "))
+        with open('expense.txt', 'a') as file:
+            file.write(f"{expense}\n")
         print(f"The Expense added is {expense}")
     elif(choice == 2):
         print("Goodbye")
