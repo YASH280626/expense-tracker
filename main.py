37b094c7f7ae431801fe9c28523d6a6e5043000e
YASH280626 <shreyanshpathak53@gmail.com>
2026-06-19 08:37:15 +0530

    Add input validation and Exception Handling

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 while True:
     print("1. Add Expense")
-    print("2. Exit")
-    choice = int(input("Enter Your Choice: "))
+    print("2. View Expense")
+    print("3. Exit")
+    try:
+        choice = int(input("Enter Your Choice: "))
+    except ValueError:
+        print("Please Enter valid value")
+        continue
     if(choice == 1):
-        expense = int(input("Enter Expense: "))
+        try:
+            expense = int(input("Enter Expense: "))
+        except ValueError:
+            print("Enter Valid Value")
+            continue
         with open('expense.txt', 'a') as file:
             file.write(f"{expense}\n")
         print(f"The Expense added is {expense}")
     elif(choice == 2):
+        try:
+            with open('expense.txt', 'r') as file:
+                for line in file:
+                    print(line.strip())
+        except FileNotFoundError:
+            print("No Expense has been saved yet!!")
+    elif(choice == 3):
+        
         print("Goodbye")
         break
     else:
