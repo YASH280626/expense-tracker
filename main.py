7933b61e30a3ffa131c2bcb04f254deea7d10726
YASH280626 <shreyanshpathak53@gmail.com>
2026-06-20 09:14:15 +0530

    Add expense categories and description

--- main.py
+++ main.py
@@ -10,17 +10,25 @@ while True:
     if(choice == 1):
         try:
             expense = int(input("Enter Expense: "))
+            category = input("Enter the category: ")
+            description = input("Enter the descriptioin: ")
         except ValueError:
             print("Enter Valid Value")
             continue
         with open('expense.txt', 'a') as file:
-            file.write(f"{expense}\n")
+            file.write(f"{expense},{category},{description}\n")
         print(f"The Expense added is {expense}")
+        print(f"The category added is {category}")
+        print(f"The descriptioin added is {description}")
     elif(choice == 2):
         try:
             with open('expense.txt', 'r') as file:
                 for line in file:
-                    print(line.strip())
+                    expense, category, description = line.strip().split(',')
+                    print(f"The expense is {expense}")
+                    print(f"The category is {category}")
+                    print(f"The description is {description}")
+                    print("-" * 20)
         except FileNotFoundError:
             print("No Expense has been saved yet!!")
     elif(choice == 3):
