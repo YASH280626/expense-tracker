e4cedadf5ff4ca7ea8b0b000b80527997476db15
YASH280626 <shreyanshpathak53@gmail.com>
2026-06-21 21:40:08 +0530

    Refractor expense tracker using functions

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+def add_expense():
+    try:
+        expense = int(input("Enter Expense: "))
+        category = input("Enter the category: ")
+        description = input("Enter the description: ")
+    except ValueError:
+        print("Enter Valid Value")
+        return
+    with open('expense.txt', 'a') as file:
+        file.write(f"{expense},{category},{description}\n")
+        print(f"The expense added is {expense}")
+        print(f"The category added is {category}")
+        print(f"The description added is {description}")
+
+def view_expenses():
+    try:
+        with open('expense.txt', 'r') as file:
+            for line in file:
+                expense, category, description = line.strip().split(',')
+                print(f"The expense is {expense}")
+                print(f"The category is {category}")
+                print(f"The description is {description}")
+                print('-' * 20)
+    except FileNotFoundError:
+        print("No Expense has been saved yet!!")
 while True:
     print("1. Add Expense")
     print("2. View Expense")
@@ -7,31 +32,11 @@ while True:
     except ValueError:
         print("Please Enter valid value")
         continue
-    if(choice == 1):
-        try:
-            expense = int(input("Enter Expense: "))
-            category = input("Enter the category: ")
-            description = input("Enter the descriptioin: ")
-        except ValueError:
-            print("Enter Valid Value")
-            continue
-        with open('expense.txt', 'a') as file:
-            file.write(f"{expense},{category},{description}\n")
-        print(f"The Expense added is {expense}")
-        print(f"The category added is {category}")
-        print(f"The descriptioin added is {description}")
-    elif(choice == 2):
-        try:
-            with open('expense.txt', 'r') as file:
-                for line in file:
-                    expense, category, description = line.strip().split(',')
-                    print(f"The expense is {expense}")
-                    print(f"The category is {category}")
-                    print(f"The description is {description}")
-                    print("-" * 20)
-        except FileNotFoundError:
-            print("No Expense has been saved yet!!")
-    elif(choice == 3):
+    if choice == 1:
+        add_expense()
+    elif choice == 2:
+        view_expenses()
+    elif choice == 3:
         
         print("Goodbye")
         break
