652c8be88bb5f8ca70aa30288d2179f90c097ed3
YASH280626 <shreyanshpathak53@gmail.com>
2026-07-17 12:24:54 +0530

    Migrate Expense Tracker from CSV to SQLite

--- main.py
+++ main.py
@@ -1,68 +1,93 @@
+import sqlite3
+connection = sqlite3.connect('expense.db')
+cursor = connection.cursor()
+# Creating the Table
+cursor.execute('''
+    Create Table If Not Exists Transactions(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               Date Text,
+               Description Text,
+               Category Text,
+               Amount Real,
+               Type Text)''')
+connection.commit()
 class ExpenseTracker:
 
-    def __init__(self):
-        self.filename = 'expense.csv'
+    def __init__(self, connection, cursor):
+        self.connection = connection
+        self.cursor = cursor
 
     def run(self):
         while True:
-            print("1. Add Expense")
-            print("2. View Expense")
+            print("1. Add Transaction")
+            print("2. View Transactions")
             print("3. Show Total Expenses")
-            print("4. Exit")
+            print("4. Show Total Income")
+            print("5. Exit")
             try:
                 choice = int(input("Enter Your Choice: "))
             except ValueError:
                 print("Please Enter Valid Value")
                 continue
             if choice == 1:
-                self.add_expense()
+                self.add_transaction()
             elif choice == 2:
-                self.view_expenses()
+                self.view_transactions()
             elif choice == 3:
                 print(f"The Total Expenses are {self.show_total_expense()}")
             elif choice == 4:
-                print("GoodBye!!")
+                print(f"The Total Income is {self.show_total_income()}")
+            elif choice == 5:
+                print("Goodbye")
                 break
+                
             else:
                 print("Enter Valid Choice")
 
     def show_total_expense(self):
-        total = 0
-        try:
-            with open(self.filename, 'r') as file:
-                for line in file:
-                    expense, category, description = line.strip().split(',')
-                    total += int(expense)
-        except FileNotFoundError:
-            print("No File Found!!")
+        self.cursor.execute('''Select SUM(Amount)
+                       from Transactions
+                       where Type = 'Expense'
+                    ''')
+        total = self.cursor.fetchone()
+        if total[0] is None:
+            return 0
             
-        return total
+        return total[0]
 
-    def add_expense(self):
+    def show_total_income(self):
+        self.cursor.execute('''Select SUM(Amount)
+                        from Transactions
+                        where Type = 'Income'
+                    ''')
+        total = self.cursor.fetchone()
+        if total[0] is None:
+            return 0
+        return total[0]
+
+    def add_transaction(self):
         try:
-            expense = int(input("Enter Expense: "))
+            date = input("Enter the Date of transaction: ")
+            amount = float(input("Enter the Amount: "))
+            transaction_type = input("Enter the Type of transaction: ")
+            
             category = input("Enter the category: ")
             description = input("Enter the description: ")
         except ValueError:
             print("Enter Valid Value")
             return
-        with open(self.filename, 'a') as file:
-            file.write(f"{expense},{category},{description}\n")
-            print(f"The expense added is {expense}")
-            print(f"The category added is {category}")
-            print(f"The description added is {description}")
+        transaction_data = (date, description, category, amount, transaction_type)
+        self.cursor.execute(''' 
+            Insert Into Transactions(Date, Description, Category, Amount, Type)
+                       values(?, ?, ?, ?, ?)
+            ''', transaction_data)
+        self.connection.commit()
 
-    def view_expenses(self):
-        try:
-            with open(self.filename, 'r') as file:
-                for line in file:
-                    expense, category, description = line.strip().split(',')
-                    print(f"The expense is {expense}")
-                    print(f"The category is {category}")
-                    print(f"The description is {description}")
-                    print('-' * 20)
-        except FileNotFoundError:
-            print("No Expense has been saved yet!!")
+    def view_transactions(self):
+        self.cursor.execute('Select * from Transactions')
+        rows = self.cursor.fetchall()
+        for row in rows:
+            print(row)
 
-tracker = ExpenseTracker()
+tracker = ExpenseTracker(connection, cursor)
 tracker.run()
